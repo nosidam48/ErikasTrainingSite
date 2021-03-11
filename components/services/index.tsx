eb0023810dc822102024bf421d3e49190092d85f
Mad Tea <43210154+nosidam48@users.noreply.github.com>
2021-03-11 19:19:12 +0100

    Add services text. Everything set

--- components/services/index.tsx
+++ components/services/index.tsx
@@ -2,6 +2,15 @@ import Image from 'next/image';
 import Link from 'next/link';
 import style from './style.module.scss';
 
+const trainingTopics = [
+  "Biting",
+  "Potty Training",
+  "Jumping",
+  "Leash Manners",
+  "Crate Training",
+  "And More..."
+];
+
 const Services = () => (
   <div className={style.services}>
     <div className={style.serviceItem}>
@@ -15,13 +24,14 @@ const Services = () => (
           alt=""
         />
       </div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-        venenatis finibus lorem non auctor. Class aptent taciti sociosqu ad
-        litora torquent per conubia nostra, per inceptos himenaeos. Nam
-        malesuada sodales magna ac consequat. Suspendisse nec turpis libero. In
-        in ultricies.
+      <p className={style.sessionText}>
+        Single Sessions start with a 90 minute consultation to assess your dogs
+        routine and needs. Erika will put together a training plan tailored to
+        your goals that will get you training right away! Follow up sessions are
+        one hour long and can be scheduled as needed.
       </p>
+      <strong className={style.sessionPrice}>First Session: $100</strong>
+      <strong>Follow ups: $90</strong>
       <Link href="/contact" passHref>
         <button className={style.button1}>Book Now!</button>
       </Link>
@@ -37,13 +47,14 @@ const Services = () => (
           alt=""
         />
       </div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-        venenatis finibus lorem non auctor. Class aptent taciti sociosqu ad
-        litora torquent per conubia nostra, per inceptos himenaeos. Nam
-        malesuada sodales magna ac consequat. Suspendisse nec turpis libero. In
-        in ultricies.
+      <p className={style.packageText}>
+        Erika’s Training Package comes with three hour-long sessions for those
+        pet parents who want continued support for their training efforts. This
+        option is great for addressing challenging behavioral issues or just
+        getting some extra support with the basics. Erika will update your
+        training plan after each session to keep you and your pup on track!
       </p>
+      <strong>Three Pack: $240</strong>
       <Link href="/contact" passHref>
         <button className={style.button2}>Book Now!</button>
       </Link>
@@ -60,12 +71,20 @@ const Services = () => (
         />
       </div>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-        venenatis finibus lorem non auctor. Class aptent taciti sociosqu ad
-        litora torquent per conubia nostra, per inceptos himenaeos. Nam
-        malesuada sodales magna ac consequat. Suspendisse nec turpis libero. In
-        in ultricies.
+        The Complete Puppy Session includes a two hours crash course on all
+        things puppy. This option is great for new puppy parents, especially
+        those with busy schedules. Get all of your questions answered and learn
+        new techniques to get your puppy started off on the right paw!
       </p>
+      <p>Topics covered include:</p>
+      <div className={style.topicList}>
+      {
+        trainingTopics.map(topic => {
+          return <div key={topic}>{topic}</div>
+        })
+      }
+      </div>
+      <strong className={style.puppySession}>Complete Puppy Session: $175</strong>
       <Link href="/contact" passHref>
         <button className={style.button3}>Book Now!</button>
       </Link>
