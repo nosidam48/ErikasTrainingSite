787a489c01536da16659817119a086c449d4a304
Mad Tea <43210154+nosidam48@users.noreply.github.com>
2021-08-26 18:18:36 +0200

    Mock next/link and image to fix tests

--- __tests__/about.test.tsx
+++ __tests__/about.test.tsx
@@ -2,6 +2,20 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import About from '../pages/about/index';
 
+jest.mock('next/link', () => ({
+  __esModule: true,
+  default: ({ children, href }) => (
+    <children.type {...children.props} href={href} />
+  ),
+}));
+
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props: any) => {
+    return <img {...props} />;
+  },
+}));
+
 it('renders about unchanged', () => {
   const tree = renderer.create(<About />).toJSON();
   expect(tree).toMatchSnapshot();
--- __tests__/services.test.tsx
+++ __tests__/services.test.tsx
@@ -2,6 +2,20 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Services from '../pages/services/index';
 
+jest.mock('next/link', () => ({
+  __esModule: true,
+  default: ({ children, href }) => (
+    <children.type {...children.props} href={href} />
+  ),
+}));
+
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props: any) => {
+    return <img {...props} />;
+  },
+}));
+
 it('renders about unchanged', () => {
   const tree = renderer.create(<Services />).toJSON();
   expect(tree).toMatchSnapshot();
